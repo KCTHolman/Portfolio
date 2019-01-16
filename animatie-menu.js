32018a910e6a593a5062670d3f2115b371d11321
KCTHolman <46706715+KCTHolman@users.noreply.github.com>
2019-01-16 22:10:45 +0100

    aanvulling inhoud per sectie

---
+++ animatie-menu.js
@@ -0,0 +1,15 @@
+
+    var hamburgerButton = document.getElementById('hamburger')
+
+    console.log(hamburgerButton)
+    hamburgerButton.addEventListener('click', functie1 )
+    var hamburger = document.querySelector('.menu');
+
+    
+    console.log(hamburger);
+    
+    function functie1(){
+    hamburger.classList.add('animate-menu');
+    };
+
+
