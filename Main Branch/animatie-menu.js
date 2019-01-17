00066e51e5037406a88744607c00c5bb7365dd5f
KCTHolman <46706715+KCTHolman@users.noreply.github.com>
2019-01-17 09:09:14 +0100

    new branch

--- Main Branch/animatie-menu.js
+++ Main Branch/animatie-menu.js
@@ -12,4 +12,16 @@
     hamburger.classList.add('animate-menu');
     };
 
+    var homeButton = document.getElementById('home-button')
+
+    console.log(hamburgerButton)
+    hamburgerButton.addEventListener('click', functie1 )
+    var hamburger = document.querySelector('.menu');
+
+    
+    console.log(hamburger);
+    
+    function functie1(){
+    hamburger.classList.add('animate-menu');
+    };
 
